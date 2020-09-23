557ff1e5636b94a04921350c6cbc4d688fc1d422
hdriqi <riqi.asdf@gmail.com>
2020-09-23 18:12:14 +0700

    add ProfileEdit page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import 'regenerator-runtime/runtime'
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import 'croppie/croppie.css'
 import './styles/tailwind.css'
 
 import ExplorePage from './pages/Explore'
 import HomePage from './pages/Home'
 import ProfilePage from './pages/Profile'
+import ProfileEditPage from './pages/ProfileEdit'
 
 export default function App() {
 	return (
@@ -17,6 +19,9 @@ export default function App() {
 				</Route>
 				<Route path="/explore" exact>
 					<ExplorePage />
+				</Route>
+				<Route path="/me/edit" exact>
+					<ProfileEditPage />
 				</Route>
 				<Route path="/:userId" exact>
 					<ProfilePage />
