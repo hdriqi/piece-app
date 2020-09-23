56e30d130c1de7d4a0e7cb340192ca1d16e50423
hdriqi <riqi.asdf@gmail.com>
2020-09-23 08:24:32 +0700

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: [],
+	theme: {
+		fontFamily: {
+			title: ['Limelight', 'cursive'],
+			body: ['Josefin Sans', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'primary-color': '#F52D3A',
+				'secondary-color': '#F7F4ED',
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
 }
