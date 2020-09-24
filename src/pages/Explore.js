b8fff3ab85a5ed93951b9ccc5397605b43f666df
hdriqi <riqi.asdf@gmail.com>
2020-09-24 09:53:02 +0700

    remove unused mock

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -2,29 +2,6 @@ import React, { useEffect, useState } from 'react'
 import ProfileCard from '../components/ProfileCard'
 import { contractGetProfileList } from '../near'
 
-// const userList = [
-// 	{
-// 		id: 'janedoe.testnet',
-// 		avatar:
-// 			'https://images.pexels.com/photos/4090852/pexels-photo-4090852.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// 		bio:
-// 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-// 	},
-// 	{
-// 		id: 'johndoe.testnet',
-// 		avatar:
-// 			'https://images.pexels.com/photos/4264824/pexels-photo-4264824.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-// 		bio:
-// 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-// 	},
-// 	{
-// 		id: 'janedoe.testnet',
-// 		avatar:
-// 			'https://images.pexels.com/photos/4177839/pexels-photo-4177839.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// 		bio: 'Write crypto',
-// 	},
-// ]
-
 const ExplorePage = () => {
 	const [userList, setUserList] = useState([])
 
