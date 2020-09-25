19204c1c64de3c4df67d5162d54f054c1c271ad2
hdriqi <riqi.asdf@gmail.com>
2020-09-25 11:07:59 +0700

    update Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,9 +13,17 @@ const LoginPage = () => {
 
 	return (
 		<div className="max-w-4xl m-auto px-4">
-			<div className="max-w-lg m-auto">
+			<div className="max-w-lg pt-24 text-center m-auto">
 				<h1 className="font-title text-3xl">PIECE</h1>
-				<button onClick={login}>Login with NEAR</button>
+				<h3 className="mt-2 text-xl text-gray-800">
+					An incentivized protocol both for creators and supporters.
+				</h3>
+				<button
+					onClick={login}
+					className="mt-8 shadow-bold font-title px-6 py-2 bg-primary-color text-white focus:outline-none"
+				>
+					Login with NEAR
+				</button>
 			</div>
 		</div>
 	)
