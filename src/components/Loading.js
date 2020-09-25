80e85c7beb849908a5a947a36817e6405359f42c
hdriqi <riqi.asdf@gmail.com>
2020-09-25 09:24:12 +0700

    add infinite scroll on Explore page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,44 @@
 import React from 'react'
 
-const Loading = () => {
+const Loading = ({ size, color }) => {
+	const styles = _styles(size, color)
 	return (
-		<div className="loading">
-			<div className="loading-child">
-				<div></div>
+		<div style={styles.loading}>
+			<div style={styles['loading-child']}>
+				<div style={styles['loading-child div']}></div>
 			</div>
 		</div>
 	)
 }
+
+const _styles = (size = 20, color = '#ffffff') => ({
+	'loading-child div': {
+		position: 'absolute',
+		width: `${size / 2}px`,
+		height: `${size / 2}px`,
+		border: `${size / 10}px solid ${color}`,
+		borderTopColor: 'transparent',
+		borderRadius: '50%',
+		animation: 'loading-spinner 1s linear infinite',
+		top: `${size / 2}px`,
+		left: `${size / 2}px`,
+		boxSizing: 'content-box',
+	},
+	loading: {
+		width: `${size}px`,
+		height: `${size}px`,
+		display: 'inline-block',
+		overflow: 'hidden',
+		background: 'none',
+	},
+	'loading-child': {
+		width: '100%',
+		height: '100%',
+		position: 'relative',
+		transform: 'translateZ(0) scale(1)',
+		backfaceVisibility: 'hidden',
+		transformOrigin: '0 0',
+	},
+})
 
 export default Loading
--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Loading from '../components/Loading'
 import ProfileCard from '../components/ProfileCard'
 import { contractGetProfileList } from '../near'
 
 const ExplorePage = () => {
 	const [userList, setUserList] = useState([])
+	const [page, setPage] = useState(0)
+	const [hasMore, setHasMore] = useState(true)
 
 	const _getProfileList = async () => {
 		const profileList = await contractGetProfileList({
-			page: 0
+			page: page,
 		})
-		setUserList(profileList)
+		const newUserList = [...userList].concat(profileList)
+		setUserList(newUserList)
+		setPage(page + 1)
+		if (profileList.length === 0 || profileList.length < 8) {
+			setHasMore(false)
+		}
 	}
 
 	useEffect(() => {
@@ -18,15 +27,29 @@ const ExplorePage = () => {
 
 	return (
 		<div className="max-w-4xl m-auto px-4">
-			<div className="flex flex-wrap -mx-4">
-				{userList.map((user) => {
-					return (
-						<div className="w-full md:w-1/4 p-4">
-							<ProfileCard user={user} />
-						</div>
-					)
-				})}
-			</div>
+			<InfiniteScroll
+				style={{
+					overflowX: 'hidden'
+				}}
+				dataLength={userList.length}
+				next={_getProfileList}
+				hasMore={hasMore}
+				loader={
+					<div className="flex items-center justify-center">
+						<Loading size={40} color={'#4a5568'} />
+					</div>
+				}
+			>
+				<div className="flex flex-wrap -mx-4">
+					{userList.map((user) => {
+						return (
+							<div className="w-full md:w-1/4 p-4">
+								<ProfileCard user={user} />
+							</div>
+						)
+					})}
+				</div>
+			</InfiniteScroll>
 		</div>
 	)
 }
